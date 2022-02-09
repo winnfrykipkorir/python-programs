95c2d7d82b174ca6fe3c2a5a7384debb6c6ade63
winnfrykipkorir <97871630+winnfrykipkorir@users.noreply.github.com>
2022-02-09 16:30:11 +0300

    Add files via upload

---
+++ finding net bonus.py
@@ -0,0 +1,14 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+#program to print the net bonus amount.
+salary=int(input("enter the salary:salary"))
+years=int(input("enter the number of years: years"))
+if years>10:
+ print(0.01*salary)
+elif years>=6 and years<=10:
+ print (0.08*salary)
+else :
+     print(0.05*salary)
+   
+ 
+   
