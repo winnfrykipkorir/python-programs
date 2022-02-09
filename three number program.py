cc7a56e6b24f77373a54c9ac5fc48d5d913a3717
winnfrykipkorir <97871630+winnfrykipkorir@users.noreply.github.com>
2022-02-09 16:30:29 +0300

    Add files via upload

---
+++ three number program.py
@@ -0,0 +1,12 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+#three number program
+a = int(input("enter a number a:"))
+b = int(input("enter a number b:"))
+c = int(input("enter a number c:"))
+if a>b and a>c :
+    print("largest number",a)
+if b>c and b>a :
+     print("largest number",b)
+if c>a and c>b:
+     print("largest number",c)
